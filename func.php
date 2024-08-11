69da3b89ef6fa338b7843321d2d9cf724e05581f
MariaGabrela <0000998473@senaimgaluno.com.br>
2024-08-10 23:08:40 -0300

    mudei imagens e calendario

--- func.php
+++ func.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login Funcionario</title>
 </head>
 <body>
 <?php
@@ -17,7 +17,7 @@ $linha = mysqli_affected_rows($conect);
 if ($linha>0){
     $num=rand(100000, 900000);
     setcookie("numlogin",$num);
-    header("Location:praguesia.php?num1='$num'");
+    header("Location:praguesias.php?num1='$num'");
 }
 else{
     echo "</br> usuario incorreto";
---
+++ praguesias.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+if (isset($_COOKIE["numlogin"])) {
+    $v1 = $_GET['num1'];
+    $v2 = $_COOKIE['numlogin'];
+
+    if ($v1 != $v2) {
+        // Redireciona para a página de login se os valores não corresponderem
+        header("Location: index.html");
+        exit;
+    } else {
+        // Se os valores corresponderem, tenta conectar ao banco de dados
+        include "conect_db.inc";
+
+        if ($conect) {
+            // Se a conexão for bem-sucedida, redireciona para a página de funcionario
+            header("Location:PagFuncionario.html");
+            mysqli_close($conect);
+            exit;
+        } else {
+            // Se a conexão não for bem-sucedida, exibe uma mensagem de erro
+            echo "<p>Usuário/Senha não encontrado</p>";
+            echo "<a href='index.html'>Voltar</a>";
+        }
+    }
+} else {
+    // Redireciona para a página de login se o cookie não estiver definido
+    header("Location: index.html");
+    exit;
+}
+?>
+</body>
+</html>
